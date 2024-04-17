0b5625cf34eb950f6a53696ffef6bab071f56ed5
Sarah Firyal <sarah5firyal@gmail.com>
2024-04-17 22:17:17 +0700

    style: landing page

---
+++ assets/scripts/script.js
@@ -0,0 +1,23 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+const navigateTo = (id) => {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth",
+    block: id == "destinations" ? "start" : "center",
+  });
+};
+
+const reveal = () => {
+  for (let i = 0; i < revealElements.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = revealElements[i].getBoundingClientRect().top;
+    let revealpoint = 0;
+
+    if (revealtop < windowheight - revealpoint) {
+      revealElements[i].classList.add("active");
+    }
+  }
+};
+window.addEventListener("scroll", reveal);
+
+reveal();
